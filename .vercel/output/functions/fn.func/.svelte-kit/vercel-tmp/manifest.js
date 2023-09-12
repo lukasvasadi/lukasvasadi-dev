67bd13ef7f5e61c04282eab024f0301377529eb4
lukasvasadi <lukas.vasadi@gmail.com>
2023-09-12 14:07:20 -0400

    Update build

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png","images/macbook-code.jpg","images/nvidia-gpu.jpg","vasadi-icon-square.png"]),
-	mimeTypes: {".png":"image/png",".jpg":"image/jpeg"},
+	assets: new Set(["images/macbook-code.jpg","images/nvidia-gpu.jpg","images/stm32.jpg","vasadi-icon-square.png"]),
+	mimeTypes: {".jpg":"image/jpeg",".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.ced4b86d.js","app":"_app/immutable/entry/app.3703133d.js","imports":["_app/immutable/entry/start.ced4b86d.js","_app/immutable/chunks/scheduler.be0e0057.js","_app/immutable/chunks/singletons.1ea6751d.js","_app/immutable/chunks/index.ef9c63d0.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.3703133d.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.be0e0057.js","_app/immutable/chunks/index.c7c9b348.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.f664d4c6.js","app":"_app/immutable/entry/app.c875f294.js","imports":["_app/immutable/entry/start.f664d4c6.js","_app/immutable/chunks/scheduler.be0e0057.js","_app/immutable/chunks/singletons.3270e733.js","_app/immutable/chunks/index.ef9c63d0.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.c875f294.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.be0e0057.js","_app/immutable/chunks/index.c7c9b348.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
