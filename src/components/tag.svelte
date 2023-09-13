f15a9915ccd3c07c35eb1434937a60e7199b41f7
lukasvasadi <lukas.vasadi@gmail.com>
2023-09-13 10:48:09 -0400

    Add tag component

---
+++ src/components/tag.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { Info } from 'lucide-svelte'
+
+	export let msg = ''
+</script>
+
+
+<p class="tag"><span><Info fill="#5d3af7" size=2rem /></span>{@html msg}</p>
+
+<style>
+    p {
+        position: relative;
+        margin-top: 2rem;
+        padding: 1rem;
+        border-radius: 10px;
+        background: #5d3af7;
+        color: var(--text-1-dark);
+    }
+
+    span {
+        z-index: 1;
+        position: absolute;
+        top: -1.2rem;
+        left: 0.5rem;
+    }
+</style>
