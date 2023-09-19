e60a7fe6a861bf765b3677d6616584abc89d06e0
lukasvasadi <lukas.vasadi@gmail.com>
2023-09-19 09:52:27 -0400

    Add Def component

---
+++ src/components/def.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { CommandList } from '$lib/types'
+
+	export let array: CommandList[]
+</script>
+
+<dl>
+	{#each array as item}
+		<dt><code>{item.command}</code></dt>
+		<dd>{item.description}</dd>
+	{/each}
+</dl>
+
+<style>
+	dl {
+		display: grid;
+		grid-template-columns: max-content auto;
+		column-gap: 1rem;
+		row-gap: 0.5rem;
+	}
+</style>
