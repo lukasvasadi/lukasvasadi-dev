7835601162a74bcf750fd6f35f73bcfc569d507a
lukasvasadi <lukas.vasadi@gmail.com>
2023-10-10 17:17:21 -0400

    Reference document image for meta tag

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.5fb0b5ad.js","_app/immutable/chunks/scheduler.ae1baad1.js","_app/immutable/chunks/index.0e1198e2.js","_app/immutable/chunks/singletons.696ae305.js","_app/immutable/chunks/index.648b4cba.js"];
+export const imports = ["_app/immutable/nodes/1.055195f8.js","_app/immutable/chunks/scheduler.ae1baad1.js","_app/immutable/chunks/index.0e1198e2.js","_app/immutable/chunks/singletons.3a2f3438.js","_app/immutable/chunks/index.648b4cba.js"];
 export const stylesheets = ["_app/immutable/assets/1.f870a4f1.css"];
 export const fonts = [];
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -9,7 +9,7 @@
 	<title>{data.meta.title}</title>
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
-	<meta property="og:image" content={'/images/macbook-code.jpg'} />
+	<meta property="og:image" content={data.meta.image} />
 </svelte:head>
 
 <article>
