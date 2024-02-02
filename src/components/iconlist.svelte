6036f4bceb73370cf5d67d7a03feacbe1dba8c01
lukasvasadi <lukas.vasadi@gmail.com>
2024-02-02 12:53:03 -0500

    Include iconlist with flexbox

---
+++ src/components/iconlist.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let html: string
+	export let desc: string
+</script>
+
+<div>
+	{@html html}
+	<p>{desc}</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		margin: 1rem 0;
+	}
+
+	div > p {
+		margin: 0;
+	}
+</style>
