47b95480fe1d606dcbee0ec2821e500ac56bfa2d
lukasvasadi <lukas.vasadi@gmail.com>
2024-02-02 12:51:14 -0500

    Include language styles for Julia

--- svelte.config.js
+++ svelte.config.js
@@ -6,26 +6,26 @@ import shiki from 'shiki'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.svelte.md', '.md', '.svx'],
-	highlight: {
-		highlighter: async (code, lang = 'text') => {
-			const highlighter = await shiki.getHighlighter({
-				theme: 'one-dark-pro',
-				lang: ['ps1', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'python', 'js']
-			})
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
-			return `{@html \`${html}\` }`
-		}
-	}
+  extensions: ['.svelte.md', '.md', '.svx'],
+  highlight: {
+    highlighter: async (code, lang = 'text') => {
+      const highlighter = await shiki.getHighlighter({
+        theme: 'one-dark-pro',
+        lang: ['ps1', 'zsh', 'bash', 'cmake', 'c', 'cpp', 'python', 'julia', 'js']
+      })
+      const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+      return `{@html \`${html}\` }`
+    }
+  }
 }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-	kit: {
-		adapter: adapter()
-	}
+  extensions: ['.svelte', '.md'],
+  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  kit: {
+    adapter: adapter()
+  }
 }
 
 export default config
